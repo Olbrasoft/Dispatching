032f7d25d20f4b653f3c322f8e6e44e445bf3fc0
MatrixStore <matrixstore@email.cz>
2020-03-16 18:50:43 +0100

    add base abstract classes Request RequestHandler

---
+++ src/Olbrasoft.Dispatching/Request.cs
@@ -0,0 +1,27 @@
+﻿using Olbrasoft.Dispatching;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Olbrasoft.Data.Cqrs
+{
+    public abstract class Request<TResponse> : IRequest<TResponse>
+    {
+        protected IDispatcher Dispatcher { get; }
+
+        protected Request()
+        {
+        }
+
+        protected Request(IDispatcher dispatcher)
+        {
+            Dispatcher = dispatcher;
+        }
+
+        protected async Task<TResponse> ExecuteWithDispatcherAsync(CancellationToken token = default)
+        {
+            return await Dispatcher.DispatchAsync(this, token);
+        }
+
+        public abstract Task<TResponse> ExecuteAsync();
+    }
+}
---
+++ src/Olbrasoft.Dispatching/RequestHandler.cs
@@ -0,0 +1,11 @@
+﻿using Olbrasoft.Dispatching;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Olbrasoft.Data.Cqrs
+{
+    public abstract class RequestHandler<TRequest, TResponse> : IRequestHandler<TRequest, TResponse> where TRequest : Request<TResponse>
+    {
+        public abstract Task<TResponse> HandleAsync(TRequest request, CancellationToken cancellationToken);
+    }
+}
